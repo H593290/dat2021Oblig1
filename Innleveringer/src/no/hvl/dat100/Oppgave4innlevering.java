7796fab9aa9c395e6e88b037c4b5a1a42ae5cb06
H593290 <69848228+H593290@users.noreply.github.com>
2021-09-10 15:36:51 +0200

    Update Oppgave4innlevering.java

--- Innleveringer/src/no/hvl/dat100/Oppgave4innlevering.java
+++ Innleveringer/src/no/hvl/dat100/Oppgave4innlevering.java
@@ -5,22 +5,22 @@ import static javax.swing.JOptionPane.*;
 public class Oppgave4innlevering {
     public static void main(String[] args) {
        int x = Integer.parseInt(showInputDialog("Skriv inn brutto lonn: "));
-       String tekst = "Med " + lån + " får du dette i trinnskatt: " ;
+       String tekst = "Med " + x + " får du dette i trinnskatt: " ;
 
        if(x <= 164000) 
        tekst += "0";
 
-     else if(x >= 164101 && lån <= 230950) 
+     else if(x >= 164101 && x <= 230950) 
      tekst += 0.93*lån/100;
 
-     else if(x >= 230951 && lån <= 580650)
+     else if(x >= 230951 && x <= 580650)
      tekst += 2.41*lån/100;
 
-     else if(x >= 580651 && lån <= 934050)
-     tekst += 11.52*lån/100;
+     else if(x >= 580651 && x <= 934050)
+     tekst += 11.52*x/100;
 
      else if(x >= 934051)
-     tekst += 14.52*lån/100;
+     tekst += 14.52*x/100;
 
      else
      tekst = "Feil ved inntasting";
